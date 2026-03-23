63d31f5bb5ffb859559178b95a31883cf1c28b0d
Anwesha Mishra <anweshamishra3986@gmail.com>
2026-03-23 18:23:41 +0530

    checking connection error

--- server/config/db.js
+++ server/config/db.js
@@ -11,6 +11,11 @@ const connectDB = async () => {
   try {
     const db = await mongoose.connect(process.env.MONGODB_URI);
     isConnected = db.connections[0].readyState === 1;
+    console.log("DB CONNECTING TO:", process.env.MONGODB_URI);
+
+    mongoose.connection.on("connected", () => {
+    console.log("DB NAME:", mongoose.connection.name);
+});
 
     console.log("Database connected");
   } catch (error) {
