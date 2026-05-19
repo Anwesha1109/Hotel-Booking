f8d7b9c6fbfdb467459a355ea541198053e4d05a
Anwesha Mishra <anweshamishra3986@gmail.com>
2026-05-19 11:16:40 +0530

    Fixed Vercel filesystem issue

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,18 +1,10 @@
 import multer from "multer";
-import path from "path";
-import fs from "fs";
-
-const uploadDir = "uploads/";
-
-// Create uploads folder if it doesn't exist
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, uploadDir);
+    cb(null, "/tmp");
   },
+
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
   },
